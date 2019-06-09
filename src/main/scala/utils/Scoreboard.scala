50b1e5c4bc4784028b8c9049417b5dcbfe489e8c
Rafał <franczakr066@gmail.com>
2019-06-09 11:24:38 +0200

    uzywanie Files do odczytu i zapisu

--- src/main/scala/utils/Scoreboard.scala
+++ src/main/scala/utils/Scoreboard.scala
@@ -1,30 +1,26 @@
 package utils
 
 import java.io._
-import java.nio.file.{Files, Paths}
-
-import scala.io.Source
+import java.nio.file.{Files, Path, Paths}
 
 class Scoreboard(val settings: GameSettings) {
-  val scoreboardPath: String = new File(".").getCanonicalPath + "/scoreboard"
+  val scoreboardPath: Path = Paths.get(new File(".").getCanonicalPath, "scoreboard")
   val filename: String = s"score${settings.maxX}x${settings.maxY}x${settings.bombsCount}.txt"
-  val filePath: String = s"${scoreboardPath.toString}/$filename"
+  val filePath: Path = Paths.get(scoreboardPath.toString, filename)
 
   var scoreboard :Array[ScoreboardItem] = Array.empty[ScoreboardItem]
   def initScoreboard(): Unit = {
     try {
-      if(!Files.exists(Paths.get(scoreboardPath)))
-        Files.createDirectory(Paths.get(scoreboardPath))
-      for (line <- Source.fromFile(filePath.toString).getLines) {
+      if(!Files.exists(scoreboardPath))
+        Files.createDirectory(scoreboardPath)
+      for (line <- Files.readAllLines(filePath).toArray(new Array[String](0))) {
         val splitted = line.split(",")
         scoreboard = Array.concat(scoreboard, Array(ScoreboardItem(splitted(0), splitted(1).toInt)))
       }
     } catch {
       case _: FileNotFoundException =>
         scoreboard = Array.empty[ScoreboardItem]
-        val fileWriter = new FileWriter(new File(filePath))
-        fileWriter.write("")
-        fileWriter.close()
+        Files.createFile(filePath)
     }
     scoreboard = scoreboard.sorted
   }
@@ -33,9 +29,7 @@ class Scoreboard(val settings: GameSettings) {
     val scoreboardItem = ScoreboardItem(name, time)
     scoreboard = Array.concat(scoreboard, Array(scoreboardItem))
     scoreboard = scoreboard.sorted.take(10)
-    val writer = new PrintWriter(new File(filePath))
-    writer.print(getScoreboardAsString)
-    writer.close()
+    Files.write(filePath, getScoreboardAsString.getBytes)
   }
 
   def getScoreboardAsString: String = {
